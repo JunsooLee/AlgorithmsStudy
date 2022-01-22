d8917d22c8d68c47e9bd9359efada22e49a4dd6a
JunsooLee <junsoolee65@naver.com>
2022-01-23 01:02:50 +0900

    Week 1_Day 1
    Brute Force problem '숫자 게임'

---
+++ 01.Brute Force/2303.숫자 게임.py
@@ -0,0 +1,23 @@
+from itertools import combinations
+
+N = int(input())
+people = []
+
+for idx in range(N):
+    card = list(map(int, input().split(' ')))
+    comb = set(combinations(card, 3))
+    max_value = 0
+    for c in comb:
+        max_value = (sum(c) % 10) if max_value <= (sum(c) % 10) else max_value
+
+    people.append(max_value)
+
+winner_value = max(people)
+
+for idx in range(N):
+    if people[idx] == winner_value:
+        winner = idx + 1
+
+print(winner)
+
+
